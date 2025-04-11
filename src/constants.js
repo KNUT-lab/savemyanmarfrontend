d976ecd8080dce466aeb5fed31ff0bff8303a596
freddie <saizaybhonehlawn@gmail.com>
2025-04-11 11:01:37 +0630

    api

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-//export const API = "https://backend.myansafe.com";
-export const API = "http://127.0.0.1:9000";
+export const API = "https://backend.myansafe.com";
+//export const API = "http://127.0.0.1:9000";
